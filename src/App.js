140a3e2c7da4bc6b5fcf7fe5a333ae9f486d3e54
Benjamin <benjaminrenny@gmail.com>
2022-05-12 17:32:21 +0100

    final changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Login from "./components/Login";
-import "./App.css"
+import "./App.css";
 import { useSelector } from "react-redux";
 import { selectUser } from "./features/userSlice";
 import Logout from "./components/Logout";
@@ -10,7 +10,7 @@ const App = () => {
 
   return (
     <div>
-      {user ? <Logout /> : <Login /> }
+      {user ? <Logout /> : <Login />}
     </div>
   );
 };
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -6,11 +6,13 @@ import { logout, selectUser } from "../features/userSlice";
 const Logout = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
+
   const handleLogout = (e) => {
     e.preventDefault();
 
     dispatch(logout());
   };
+
 
   return (
     <div className="logout">
@@ -19,9 +21,6 @@ const Logout = () => {
       </h1>
       <button className="logout_btn" onClick={(e) => handleLogout(e)}>
         Logout
-      </button>
-      <button className="diff_userbtn">
-        <a href="/">Sign In with Different User</a>
       </button>
     </div>
   );
